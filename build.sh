3168931acb7d99167f0082302203b08fa1da1712
José Albornoz <jojo@eljojo.net>
2021-01-31 03:02:44 -0500

    make build script a bit more descriptive

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 export PATH=$PATH:/usr/local/go/bin
 cd "$(dirname "$(realpath "$0")")";
 
+echo "=> building nara on $(hostname)"
 git pull
 ./get_deps.sh
+echo "building"
 go build nara.go
+echo "restarting service"
 sudo systemctl restart nara
