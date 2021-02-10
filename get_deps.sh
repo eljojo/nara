a466867bd71c2302ae1d0b0551ac4444b06b019e
José Albornoz <jojo@eljojo.net>
2021-02-09 22:23:39 -0500

    avoid get deps

--- get_deps.sh
+++ get_deps.sh
@@ -2,7 +2,7 @@ echo "[$(hostname)] getting dependencies"
 go get github.com/eclipse/paho.mqtt.golang
 go get github.com/sirupsen/logrus
 go get github.com/go-ping/ping
-go get -u github.com/shirou/gopsutil/host
-go get -u github.com/shirou/gopsutil/load
+# go get -u github.com/shirou/gopsutil/host
+# go get -u github.com/shirou/gopsutil/load
 go get github.com/lensesio/tableprinter
-go get -u golang.org/x/sys/unix
+# go get -u golang.org/x/sys/unix
