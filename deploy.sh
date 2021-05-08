03b20c30878b341f654d91f9481c5243a79242c7
José Albornoz <jojo@eljojo.net>
2021-05-08 03:22:23 -0400

    commit deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+git push
+
+deployMachines=(dietpi@desk-pi.eljojo.dev lisa.eljojo.casa bloo.eljojo.net dietpi@music-station.eljojo.dev dietpi@cayumanqui.eljojo.dev dietpi@music-pi.eljojo.dev  pi@inky.eljojo.dev)
+
+echo "deploying nara..."
+
+for m in ${deployMachines[@]}; do
+  ssh $m "cd ~/nara && git checkout -f master"
+done
+
+git push deploy -f master:deploy
+
+for m in ${deployMachines[@]}; do
+  echo "\n\n$m:"
+  ssh $m "cd ~/nara && git checkout -f deploy"
+  ssh $m '~/nara/build.sh'
+  echo "sleeping for 65 seconds to stabilize narae memory"
+  sleep 65
+done
