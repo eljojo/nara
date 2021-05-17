342b967d1a7a2f9b7a9dd20526d14629846e8393
José Albornoz <jojo@eljojo.net>
2021-05-17 19:17:58 -0400

    use nicer frontend

--- web/public/nara.js
+++ web/public/nara.js
@@ -1,62 +1,65 @@
 'use strict';
 
-const e = React.createElement;
+function NaraRow(props) {
+  const nara = props.nara;
+  const ver = nara.Verdict
 
-class NaraList extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.state = { Name: 'jojo', HostStats: {} };
-  }
+  return (
+    <tr>
+      <td>{ nara.Name }</td>
+      <td>{ nara.Barrio }</td>
+      <td>{ nara.Verdict.Online }</td>
+      <td>{ moment(ver.LastSeen * 1000).fromNow() }</td>
+      <td>{ moment(ver.LastRestart * 1000).fromNow(true) }</td>
+      <td>{ moment(ver.StartTime * 1000).fromNow(true) }</td>
+      <td>{ ver.Restarts }</td>
+      <td>{ moment().to((moment().unix() - nara.HostStats.Uptime) * 1000, true)  }</td>
+    </tr>
+  );
+}
 
-  render() {
-    const renderNara = function(name, nara) {
-      return (
+function NaraList() {
+  const { useState, useEffect } = React;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const refresh = () => {
+      window.fetch("/api.json")
+        .then(response => response.json())
+        .then(root => {
+          const data = Object.entries(root).map(item => (Object.assign(item[1], {
+            Name: item[0],
+          })));
+          setData(data);
+        });
+    };
+    refresh();
+    const interval = setInterval(refresh, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <table id="naras">
+      <thead>
         <tr>
-        <td>{ name }</td>
-        <td>{ nara.Barrio }</td>
-        <td>{ nara.Verdict.Online }</td>
-        <td>{ moment(nara.Verdict.LastSeen * 1000).fromNow() }</td>
-        <td>{ moment(nara.Verdict.LastRestart * 1000).fromNow(true) }</td>
-        <td>{ moment(nara.Verdict.StartTime * 1000).fromNow(true) }</td>
-        <td>{ nara.Verdict.Restarts }</td>
-        <td>{ moment().to((moment().unix() - nara.HostStats.Uptime) * 1000, true)  }</td>
+          <th>Name</th>
+          <th>Neighbourhood</th>
+          <th>Nara</th>
+          <th>Last Ping</th>
+          <th>Nara Uptime</th>
+          <th>Nara Lifetime</th>
+          <th>Restarts</th>
+          <th>Host Uptime</th>
         </tr>
-      );
-    }
-
-    const naraData = this.props.naras;
-    const naras = Object.keys(naraData).map((name, index) =>
-      renderNara(name, naraData[name])
-    );
-
-    return (
-      <table id="naras">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Neighbourhood</th>
-            <th>Nara</th>
-            <th>Last Ping</th>
-            <th>Nara Uptime</th>
-            <th>Nara Lifetime</th>
-            <th>Restarts</th>
-            <th>Host Uptime</th>
-          </tr>
-        </thead>
-        <tbody>{ naras }</tbody>
-      </table>
-    );
-  }
+      </thead>
+      <tbody>{
+        data.map((nara) =>
+        <NaraRow nara={nara} key={nara.Name} />
+      )
+      }</tbody>
+    </table>
+  );
 }
 
 const domContainer = document.querySelector('#naras_container');
-var updateNaras = function(data) {
-  ReactDOM.render(e(NaraList, { naras: data }), domContainer);
-};
-var refreshNow = function() {
-  fetch('/api.json')
-    .then(response => response.json())
-    .then(updateNaras);
-};
-refreshNow();
-setInterval(refreshNow, 1000);
+ReactDOM.render(e(NaraList), domContainer);
