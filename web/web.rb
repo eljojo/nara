bf5d3562d731cbddc39dd70c655718037e28eedd
José Albornoz <jojo@eljojo.net>
2021-05-22 01:33:49 -0400

    try to populate local database with info from observations

--- web/web.rb
+++ web/web.rb
@@ -32,38 +32,27 @@ class NaraWeb
       @db[name]["Name"] = name
       @db[name]["LastSeen"] = Time.now.to_i
 
-      calculate_verdict!
-      @db = @db.to_a.sort_by { |name, data| [data.dig("Verdict", "ClusterName"), name].compact }.to_h
-    end
-  end
+      @db.dup.each do |n, entry|
+        observations = entry["Observations"]
+        observations.each do |nn, o|
+          next if @db.key?(nn)
+          @db[nn] = {}
+          @db[nn]["Name"] = nn
+          @db[nn]["Barrio"] = o["ClusterName"]
+          @db[nn]["LastSeen"] = o["LastSeen"]
+          @db[nn]["Observations"] = { nn => o.dup }
+          @db[nn]["HostStats"] = { "Uptime" => 0 }
+        end
+      end
 
-  private
+      @db.each do |n, entry|
+        observations = entry["Observations"]
+        time_last_seen = Time.now.to_i - entry["LastSeen"]
+        observations[n]["Online"] = "MISSING" if time_last_seen > 60
+      end
 
-  def calculate_verdict!
-    @db.each do |name, nara|
-      obs = observations_for(name)
-      nara["Verdict"] = sum_verdict(obs)
+      @db = @db.to_a.sort_by { |name, data| [data.fetch("Barrio", name), name] }.to_h
     end
-  end
-
-  def observations_for(name)
-    res = []
-    @db.each do |n_name, nara|
-      next if name == n_name
-      obs = nara["Observations"][name]
-      res << obs if obs
-    end
-    res
-  end
-
-  def sum_verdict(obs)
-    verdict = {}
-    keys = obs.flat_map(&:keys).uniq
-    keys.each do |key|
-      values = obs.select{|o| o.key?(key) }.map { |o| o[key] }
-      verdict[key] = values.max_by { |value| values.count(value) } 
-    end
-    verdict
   end
 end
 
