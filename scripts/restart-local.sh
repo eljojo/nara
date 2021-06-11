fa12c757aba11338b159eca9ee35991ebb2d9c67
José Albornoz <jojo@eljojo.net>
2021-06-10 23:27:20 -0400

    try to fix restart script

--- scripts/restart-local.sh
+++ scripts/restart-local.sh
@@ -1,11 +1,17 @@
+set -e
 echo "~  restarting nara on $(hostname) ~"
 
 echo "-> querying nara api..."
 machines=$(curl --silent https://nara.eljojo.net/api.json|jq -r '.naras | map(.Name) | .[]')
 
 for name in ${machines[@]}; do
-  systemctl restart nara-$name && echo "restarted  $name" || true
-  if [[ "$name" != "music-station" &&  "$name" != "music-pi" &&  "$name" != "cayumanqui" &&  "$name" != "desk-pi" ]]; then
-    systemctl restart nara-web
+  if systemctl show nara-$name > /dev/null; then
+    systemctl restart nara-$name
+    echo "restarted $name"
   fi
 done
+
+if systemctl show nara-web > /dev/null; then
+  systemctl restart nara-web
+  echo "restarted nara-web"
+fi
