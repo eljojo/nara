97d246e1a862598298948a4168e21865df323cad
José Albornoz <jojo@eljojo.net>
2021-06-10 20:56:20 -0400

    silence error on build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ echo "-> local build on $(hostname)"
 arch=$(uname -m)
 if [ "$arch" == 'aarch64' ]; then
   echo "applying panicwrap linux hotfix lmao"
-  sudo rm $HOME/go/pkg/mod/github.com/bugsnag/panicwrap@v1.3.2/dup2.go || true
+  sudo rm $HOME/go/pkg/mod/github.com/bugsnag/panicwrap@v1.3.2/dup2.go 2>/dev/null
 fi
 go build -o ../build/nara ../cmd/nara/main.go
