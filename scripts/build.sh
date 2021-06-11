4f8ff7a498b217734443d29b3b2206b8eb8a1f29
José Albornoz <jojo@eljojo.net>
2021-06-10 21:42:56 -0400

    fix build script for some machines

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,9 @@ echo "-> local build on $(hostname)"
 arch=$(uname -m)
 if [ "$arch" == 'aarch64' ]; then
   echo "applying panicwrap linux hotfix lmao"
-  sudo rm $HOME/go/pkg/mod/github.com/bugsnag/panicwrap@v1.3.2/dup2.go 2>/dev/null
+  if [ -z "$GOPATH" ]; then
+    export GOPATH="$HOME/go"
+  fi
+  sudo rm $GOPATH/pkg/mod/github.com/bugsnag/panicwrap@v1.3.2/dup2.go 2>/dev/null
 fi
 go build -o ../build/nara ../cmd/nara/main.go
