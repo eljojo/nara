adb497f82b7f19aa29a8fc68012f354c07a1c469
José Albornoz <jojo@eljojo.net>
2021-06-17 01:37:00 -0400

    don't restart nara web and try to silence errors in restart script

--- scripts/restart-local.sh
+++ scripts/restart-local.sh
@@ -2,13 +2,8 @@ set -e
 echo "~  restarting nara on $(hostname) ~"
 
 for name in "$@"; do
-  if systemctl is-enabled --quiet nara-$name; then
+  if systemctl is-enabled --quiet nara-$name 2> /dev/null; then
     systemctl restart nara-$name
     echo "restarted $name"
   fi
 done
-
-if systemctl is-enabled --quiet nara-web; then
-  systemctl restart nara-web
-  echo "restarted nara-web"
-fi
