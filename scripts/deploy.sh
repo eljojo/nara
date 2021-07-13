e6bc95e0e77f57040d8d27e04552df41c8aaea1a
José Albornoz <jojo@eljojo.net>
2021-07-13 16:43:12 -0400

    fork nara-web into its own repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,12 @@ echo "~  deploying nara version $NARA_VERSION ~"
 
 echo "-> querying nara api..."
 machines=$(curl --silent https://nara.eljojo.net/narae.json|jq -r '.naras | map(.Name) | .[]')
-machines_in_one_line=$(echo "$machines"|tr '\n' ' ')
+echo "deploying to" $machines
 
 naraSsh () {
   local m="$1.eljojo.dev"
   if [[ "$1" == "music-station" ||  "$1" == "music-pi" ||  "$1" == "cayumanqui" ||  "$1" == "desk-pi" ]]; then
     m="dietpi@$m"
-  elif [[ "$1" == "burrito" ]]; then
-    m="eljojo@$m"
   else
     m="jojo@$m"
   fi
@@ -20,10 +18,9 @@ naraSsh () {
   echo "$m"
 }
 
-if grep -Fq 'remote "deploy"' .git/config; then # naive check to see there's a deploy upstream
-  git remote remove deploy
+if ! grep -Fq 'remote "deploy"' .git/config; then # naive check to see there's a deploy upstream
+  git remote add deploy jojo@lisa.eljojo.casa:nara
 fi
-git remote add deploy jojo@lisa.eljojo.casa:nara
 
 for name in ${machines[@]}; do
   m=$(naraSsh $name)
@@ -40,29 +37,17 @@ echo "pushing code to all machines"
 git push deploy -f "HEAD:deploy" -q
 
 for name in ${machines[@]}; do
-  if [[ "$1" != "" ]]; then
-    if [[ $(echo "$name" | grep -E "$1") == "" ]]; then
-      echo "skipping $name"
-      delete=$name
-      machines=( "${machines[@]/$delete}" )
-    fi
-  fi
-done
-
-echo "deploying to" $machines
-for name in ${machines[@]}; do
   m=$(naraSsh $name)
 
   if ! nslookup "$name.eljojo.dev" > /dev/null; then
-    continue
+    echo "skipping $name" && continue
   fi
 
-  echo "=> deploying nara on $name"
-  ssh -q $m "cd ~/nara && git checkout -f $NARA_VERSION -q"
-  ssh -q $m '~/nara/scripts/build.sh'
-  ssh -q $m "sudo ~/nara/scripts/restart-local.sh $machines_in_one_line"
-
-  echo "=> succesfully deployed $name"
+  if [[ "$name" != "music-station" &&  "$name" != "music-pi" &&  "$name" != "cayumanqui" &&  "$name" != "desk-pi" &&  "$name" != "burrito" ]]; then
+    echo "=> deploying nara-web on $name"
+    ssh -q $m "cd ~/nara && git checkout -f $NARA_VERSION -q"
+    ssh -q $m "sudo systemctl restart nara-web"
+  fi
 done
 
 echo "nara deployed 🎉 congrats :-)"
