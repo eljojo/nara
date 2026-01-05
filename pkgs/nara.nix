b09318cb47c017f982d52bff90cf0b8a50ef96a6
José Albornoz <jojo@eljojo.net>
2026-01-04 22:21:37 -0500

    new hash after vendor tiny mod

--- pkgs/nara.nix
+++ pkgs/nara.nix
@@ -4,7 +4,7 @@ buildGoModule {
   pname = "nara";
   version = "latest";
   src = lib.cleanSource ../.;
-  vendorHash = "sha256-4hITFVO4/OhwdzdtfqB/kc51nPCkNfoakZvl5HaB/Kc=";
+  vendorHash = "sha256-XqceohRq03OMny8VX1RHBRYtY68VHVG9K2KVa5Ra4a8=";
   subPackages = [ "cmd/nara" ];
 
   meta = with lib; {
