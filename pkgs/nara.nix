dc56eaa4c43400383f4603165641160e8552d2ab
José Albornoz <jojo@eljojo.net>
2026-01-08 02:12:07 -0500

    fix hash again

--- pkgs/nara.nix
+++ pkgs/nara.nix
@@ -4,7 +4,7 @@ buildGoModule {
   pname = "nara";
   version = "latest";
   src = lib.cleanSource ../.;
-  vendorHash = "sha256-Bz6wq2zF9h9fKfP4bvKk8P4GQs60gVsyaNp4alF7L1M=";
+  vendorHash = "sha256-ZlI5zbq3CzaoRgrR/lYv5lv2leipFxQHT5shV6BzrBg=";
   subPackages = [ "cmd/nara" ];
 
   meta = with lib; {
